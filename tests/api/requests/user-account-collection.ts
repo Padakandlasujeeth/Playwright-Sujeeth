13fd1e4713af7ef531dba7dc2d067459c3bc3e5a
Padakandlasujeeth <saisujeethpadakandla@gmail.com>
2023-10-16 12:55:34 -0700

    Updating Request URL

--- tests/api/requests/user-account-collection.ts
+++ tests/api/requests/user-account-collection.ts
@@ -6,7 +6,7 @@ import { testConfig } from '../../../testConfig';
 async function createUser(apiContext: APIRequestContext, userName: string, password: string) {
   const method = methods.post;
   const requestOptions = {data: {userName:userName,password:password}};
-  const requestUrl = testConfig.qa+"Account/v1/User";
+  const requestUrl = testConfig.qa+"/Account/v1/User";
   const response = await executeRequest(apiContext, requestUrl, method, requestOptions);
 }
 
